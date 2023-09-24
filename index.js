4bb1f6a41dafeea5fff06cd0480cc196487cbef7
Nahason <nahason.murithi@student.moringaschool.com>
2023-09-24 07:52:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names){
+    for (let i = 0 ; i < names.length; i++){
+        let message =`Thank you, ${names[i]}, for the wonderful surprise gifts!`;
+    }
+    return writeCards;
+}
+
+function countDown(){
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+};
